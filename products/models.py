df61f765f22709bb20d05613051310d5b9a52603
Wonbin Lee <lob3767@gmail.com>
2022-03-13 00:35:18 +0900

    Modict: datatable models(Category)

--- products/models.py
+++ products/models.py
@@ -10,7 +10,7 @@ class Menu(models.Model):
 
 class Category(models.Model):
     name = models.CharField(max_length=45)
-    menu = models.ForeignKey("Mengeru", on_delete=models.CASCADE)
+    menu = models.ForeignKey("Menu", on_delete=models.CASCADE)
 
     class Meta:
         db_table = "categories"
